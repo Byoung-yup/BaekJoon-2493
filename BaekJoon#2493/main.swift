d8a27ea2582d8d443c59ef1c6f145fdb582d7a6a
Dev_Ozzy <20170926@vision.hoseo.edu>
2023-03-24 02:15:53 +0900

    commit

--- BaekJoon#2493/main.swift
+++ BaekJoon#2493/main.swift
@@ -7,5 +7,23 @@
 
 import Foundation
 
-print("Hello, World!")
+func solution() {
+    
+    let n = Int(readLine()!)!
+    let top = readLine()!.split(separator: " ").map { Int(String($0))! }
+    
+    var stack = [Int]()
+    var ans = Array(repeating: 0, count: n)
+    
+    for i in stride(from: n - 1, to: -1, by: -1) {
+        while !stack.isEmpty && top[i] > top[stack.last!] {
+            let idx = stack.removeLast()
+            ans[idx] = i + 1
+        }
+        stack.append(i)
+    }
+    
+    print(ans.map { String($0) }.joined(separator: " "))
+}
 
+solution()
